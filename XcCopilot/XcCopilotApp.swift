8c58e4845cf92977bea2bec9bd43035fd0604226
Dave Cameron <dave@tinyweb.ca>
2023-12-04 21:38:46 -0500

    Initial Commit

---
+++ XcCopilot/XcCopilotApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcCopilotApp.swift
+//  XcCopilot
+//
+//  Created by Dave Cameron on 2023-12-04.
+//
+
+import SwiftUI
+
+@main
+struct XcCopilotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
